88785c5ab1e66f58657c05e1f18b6868b821840b
Vsevolod <mr.seva.021@gmail.com>
2022-12-13 17:30:58 +0300

    arraymethods4

--- Module5-data-types/5.5.arrayMethods.js
+++ Module5-data-types/5.5.arrayMethods.js
@@ -121,8 +121,6 @@ cars.sort();
 console.log(cars);
 */
 
-let numbers = [2, 324,  132, 113, 73, 20912, 111];
-
 // let compareNumeric = (a, b) => a - b; 
 
 // numbers.sort(compareNumeric);
@@ -180,7 +178,7 @@ console.log(soldiers);
 
 // TASKS
 // task #1
-/*
+
 function camelize(str) {
   let arr = str.split('-');
   arr = arr.map((elem, index) => {
@@ -196,10 +194,11 @@ console.log(camelize("background-color"));
 console.log(camelize("list-style-image"));
 console.log(camelize("-webkit-transition"));
 console.log(camelize('background-color'));
-*/
+
+
 
 // task #2
-/*
+
 function filterRange(arr, a, b)  {
   return arr.filter(elem => elem >= Math.min(a, b) && elem <= Math.max(a, b));
 }
@@ -211,11 +210,11 @@ let filtered = filterRange(arr, 1, 4);
 alert( filtered ); // 3,1 (совпадающие значения)
 
 alert( arr ); // 5,3,8,1 (без изменений)
-*/
+
 
 
 // task #3
-/*
+
 function filterRangeInPlace(arr, a, b) {
 
   for (let i = 0; i < arr.length; i++) {
@@ -235,18 +234,20 @@ let arr = [5, 3, 8, 1];
 filterRangeInPlace(arr, 1, 4); // удалены числа вне диапазона 1..4
 
 alert( arr ); // [3, 1]
-*/
+
+
 
 // task #4
-/*
+
 let arr = [5, 2, -1, 14, 17];
 
 arr.sort((a, b) => b - a);
 alert(arr);
-*/
+
+
 
 // task #5
-/*
+
 let arr = ["HTML", "JavaScript", "CSS"];
 
 function copySorted(arr) {
@@ -256,35 +257,32 @@ function copySorted(arr) {
 let sorted = copySorted(arr);
 alert( sorted ); // CSS, HTML, JavaScript
 alert( arr ); // HTML, JavaScript, CSS (без изменений)
-*/
+
+
 
 // task #6
-function Calculator () {
-    this.methods = {
-        '+': (a, b) => a + b,
-        '-': (a, b) => a - b,
-    };
 
-    this.calculate = function(str) {
-        a = +str.split(' ')[0];
-        b = +str.split(' ')[2];
-        oper = str.split(' ')[1];
+function Calculator() {
+  this.methods = {
+    '+': (a, b) => a + b,
+    '-': (a, b) => a - b,
+  };
 
-        if (oper == '+') {
-            return a + b;
-        } else if (oper == '-') {
-            return a - b;
-        }
+  this.calculate = function (str) {
+    a = +str.split(' ')[0];
+    b = +str.split(' ')[2];
+    oper = str.split(' ')[1];
 
-        if (oper in this.methods) {
-            return this.methods[oper](a, b);
-        }
-    };
+    if (oper in this.methods) {
+      return this.methods[oper](a, b);
+    }
+  };
 
-    this.addMethod = function(oper, method) {
-        this.methods[oper] = method;
-    };
+  this.addMethod = function (oper, method) {
+    this.methods[oper] = method;
+  };
 }
+
 
 let powerCalc = new Calculator;
 powerCalc.addMethod("*", (a, b) => a * b);
@@ -293,18 +291,169 @@ powerCalc.addMethod("**", (a, b) => a ** b);
 
 let result = powerCalc.calculate("2 ** 3");
 let result1 = powerCalc.calculate('1 + 3')
-alert( result1 ); // 8
+alert(result1); // 8
 
+
+
+// task #7
+
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 28 };
+
+let users = [ vasya, petya, masha ];
+
+let names = users.map(item => item.name);
+
+alert( names ); // Вася, Петя, Маша
+
+
+
+// task #8
+
+let vasya = { name: "Вася", surname: "Пупкин", id: 1 };
+let petya = { name: "Петя", surname: "Иванов", id: 2 };
+let masha = { name: "Маша", surname: "Петрова", id: 3 };
+
+let users = [ vasya, petya, masha ];
+
+let usersMapped = users.map(item => {
+  return { fullName: item.name + ' ' + item.surname, id: item.id }
+});
+  
+
+alert( usersMapped[0].id ) // 1
+alert( usersMapped[0].fullName ) // Вася Пупкин
+
+
+// task #9 
+
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 28 };
+
+let sortByAge = (arr) => {
+  arr.sort((a, b) => a.age > b.age ? 1 : -1);
+};
+
+let arr = [ vasya, petya, masha ];
+
+sortByAge(arr);
+
+// теперь: [vasya, masha, petya]
+alert(arr[0].name); // Вася
+alert(arr[1].name); // Маша
+alert(arr[2].name); // Петя
+
+
+
+// task #10
+
+let arr = [1, 2, 3];
+
+// function shuffle(arr) {
+//   arr.sort(() => (Math.random() >= 0.5) ? 1 : -1 );
+//   alert(arr);
+// }
+
+// function shuffle(arr) {
+//   arr.sort(() => (Math.random() - 0.5));
+//   alert(arr);
+// }
+
+// Алгоритм Фишера-Йетса тасовки
+function shuffle(arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+
+    [arr[j], arr[i]] = [arr[i], arr[j]];
+  }
+}
+
+shuffle(arr);
+alert(arr);
+
+
+
+// task #11
+
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 29 };
+
+function getAverageAge(arr) {
+  return arr.reduce((sum, current) => sum + current.age, 0) / arr.length
+}
+
+let arr = [ vasya, petya, masha ];
+
+alert( getAverageAge(arr) ); // (25 + 30 + 29) / 3 = 28
+
+
+
+// task #12
+
+function unique(arr) {
+  let set = [];
+  for (let elem of arr) {
+    if (!set.includes(elem)) {
+      set.push(elem);
+    }
+  }
+
+  return set;
+}
+
+let strings = ["кришна", "кришна", "харе", "харе",
+  "харе", "харе", "кришна", "кришна", ":-O"
+];
+
+alert( unique(strings) ); // кришна, харе, :-O
+
+
+// task #13
+let users = [
+  {id: 'john', name: "John Smith", age: 20},
+  {id: 'ann', name: "Ann Smith", age: 24},
+  {id: 'pete', name: "Pete Peterson", age: 31},
+];
+
+function groupById(users) {
+
+  return users.reduce((prev, elem) => {
+    prev[elem.id] = elem;
+    return prev;
+  }, {})
+
+  // for(let elem of users) {
+  //   usersById[elem.id] = elem;
+  // }
+
+  // return(usersById);
+}
+
+let usersById = groupById(users);
+
+console.log(usersById);
+/*
+// после вызова у нас должно получиться:
+
+usersById = {
+  john: {id: 'john', name: "John Smith", age: 20},
+  ann: {id: 'ann', name: "Ann Smith", age: 24},
+  pete: {id: 'pete', name: "Pete Peterson", age: 31},
+}
+*/
 
 // arr.splice(start, deleteCount, add, ...) - добавляет или удаляет элементы внутри массива, deleteCount - кол-воо удаляемых элементов
 // arr2 = arr.slice(start, end) - копирует фрагмент массива - ВОЗВРАЩАЕТ В ДРУГОЙ МАССИВ!
 // arrN - arr.concat(arr2, arr3, ...) - копирует массив + доп массивы (эл-ты) - ВОЗВРАЩАЕТ В ДРУГОЙ МАССИВ!
 // arr.forEach(function(elem, index, array)) - цикл массива
-// arr.indexOf(elem, from) - индекс элемента elem с from (иначе -1) 
-// arr.lastIndexOf(elem, from) - последний индекс элемента elem с from (иначе -1) 
+// arr.indexOf(elem, from) - индекс элемента elem с from (иначе -1)
+// arr.lastIndexOf(elem, from) - последний индекс элемента elem с from (иначе -1)
 // arr.includes(elem, from) - вхождение элемента в массив с from
 // elem = arr.find(function(elem, index, array)) - ВОЗВРАЩАЕТ ПОДХОДЯЩИЙ УСЛОВИЮ ЭЛЕМЕНТ!
-// arr2 = arr.filter(funcction(elem, index, array)) - копирует все элементы, подходящие условию - ВОЗВРАЩЕТ В ДРУГОЙ МАССИВ!  
+// arr2 = arr.filter(funcction(elem, index, array)) - копирует все элементы, подходящие условию - ВОЗВРАЩЕТ В ДРУГОЙ МАССИВ!
 // arr2 = arr.map(funcction(elem, index, array)) - копирует возвращаемое значание - ВОЗВРАЩАЕТ В ДРУГОЙ МАССИВ!
 // arr.sort(function()) - сортировка массива, function() задает правило сортировки
 // arr.reverse() - меняет порядок элементов
