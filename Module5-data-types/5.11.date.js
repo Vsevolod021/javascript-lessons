0f1d7982ccfce373a83cb10e482684a7adbd7425
Vsevolod <mr.seva.021@gmail.com>
2022-12-17 14:55:01 +0300

    dates completed

--- Module5-data-types/5.11.date.js
+++ Module5-data-types/5.11.date.js
@@ -116,11 +116,38 @@ alert(thisYear);
 
 
 // task #7
-function getSecondsToTomorrow() {
-    let now = new Date();
-    let tomorrow = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1);
+// function getSecondsToTomorrow() {
+//     let now = new Date();
+//     let tomorrow = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1);
 
-    return Math.ceil((tomorrow.getTime() - now.getTime()) / 1000);
+//     return Math.ceil((tomorrow.getTime() - now.getTime()) / 1000);
+// }
+
+// console.log(getSecondsToTomorrow());
+
+
+// task #8
+function formatDate(date) {
+    let difference = Date.now() - date.getTime();
+    if (difference < 1000) {
+        return 'прямо сейчас';
+    } else if (difference < 1000 * 60) {
+        return `${Math.round(difference/1000)} сек. назад`;
+    } else if (difference < 1000 * 60 * 60) {
+        return `${Math.round(difference/(1000 * 60))} мин. назад`;
+    } else {
+        return `${date.getDate()}.${date.getMonth()+1}.${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`;
+    }
 }
 
-console.log(getSecondsToTomorrow());
+
+console.log( formatDate(new Date(new Date - 1)) ); // "прямо сейчас"
+
+console.log( formatDate(new Date(new Date - 30 * 1000)) ); // "30 сек. назад"
+
+console.log( formatDate(new Date(new Date - 5 * 60 * 1000)) ); // "5 мин. назад"
+
+// вчерашняя дата вроде 31.12.2016, 20:00
+console.log( formatDate(new Date(new Date - 86400 * 1000)) );
+
+// console.log(new Date);
