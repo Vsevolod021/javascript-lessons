47d052cd2dd6d68cfbb84ce731b02bb1f7dfd9f0
Vsevolod <mr.seva.021@gmail.com>
2023-11-29 12:56:04 +0300

    classes

---
+++ Basis/Module9-classes/9.2.classInheritance.js
@@ -0,0 +1,50 @@
+/*
+class Animal {
+  constructor(name) {
+    this.speed = 0;
+    this.name = name;
+  }
+  run(speed) {
+    this.speed = speed;
+    alert(`${this.name} бежит со скоростью ${this.speed}.`);
+  }
+  stop() {
+    this.speed = 0;
+    console.log(`${this.name} стоит неподвижно.`);
+  }
+}
+
+class Rabbit extends Animal {
+  constructor(name, earlength) {
+    this.speed = 0;
+    this.name = name;
+    this.
+  }
+}
+
+let rabbit = new Rabbit('Крош');
+rabbit.stop();
+
+class Animal {
+  name = 'animal';
+
+  constructor() {
+    alert(this.name); // (*)
+  }
+
+  showName() {
+    console.log(this.name);
+  }
+}
+
+class Rabbit extends Animal {
+  constructor() {
+    super(name);
+    this.name = 'rabbit';
+  }
+}
+
+new Animal(); // animal
+new Rabbit().showName(); // animal
+
+*/
